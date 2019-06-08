0d333b9892e17773ba014c24eeef48ed52b24141
andyWang <295268319@qq.com>
2019-06-08 21:15:14 +0800

    将RpcInvocationHandler和RpcProxy合二为一

--- rpc-client/src/main/java/com/andy/rpc/client/RpcProxy.java
+++ rpc-client/src/main/java/com/andy/rpc/client/RpcProxy.java
@@ -1,5 +1,10 @@
 package com.andy.rpc.client;
 
+import com.andy.rpc.server.api.RpcRequest;
+import com.andy.rpc.server.api.SerializableUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
@@ -15,4 +20,29 @@ public class RpcProxy {
                 new Class[]{service},
                 new RpcInvocationHandler());
     }
+
+    class RpcInvocationHandler implements InvocationHandler {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            RpcRequest request = new RpcRequest();
+            //jdk 动态代理
+            Class<?>[] classes = proxy.getClass().getInterfaces();
+            String className = classes[0].getName();
+            request.setServerName(className);
+            request.setArgs(args);
+            request.setMethod(method.getName());
+            String [] types = null;
+            if(args!=null) {
+                types = new String [args.length];
+                for (int i = 0; i < types.length; i++) {
+                    types[i] = args[i].getClass().getName();
+                }
+            }
+            request.setTypes(types);
+            byte[] byteArray = SerializableUtils.serialized(request);
+            return RpcClient.send(byteArray);
+        }
+    }
 }
+
+
